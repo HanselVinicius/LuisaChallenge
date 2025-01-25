53780f6f611598f712142f54e4ed1a31850cf5ba
Vinicius Hansel <hanelvinicius@gmail.com>
2025-01-25 11:39:04 -0300

    feat(Client.kt) changes on modeling

--- src/main/kotlin/com/vinicius/challenge/core/domain/client/Client.kt
+++ src/main/kotlin/com/vinicius/challenge/core/domain/client/Client.kt
@@ -6,6 +6,6 @@ class Client(
     val id: Long?,
     val name: String,
     val auth: Auth,
-    val favoriteList: FavoriteList? = null,
+    var favoriteList: FavoriteList? = null,
     var enabled: Boolean
 )
