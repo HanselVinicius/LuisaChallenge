887495c53ca0f034cdc4db275b2bf5221a2f9b52
Vinicius Hansel <hanelvinicius@gmail.com>
2025-01-25 19:52:48 -0300

    feat(TokenDto.kt) changed authController return

--- src/main/kotlin/com/vinicius/challenge/entrypoint/auth/dto/TokenDto.kt
+++ src/main/kotlin/com/vinicius/challenge/entrypoint/auth/dto/TokenDto.kt
@@ -1,9 +1,8 @@
 package com.vinicius.challenge.entrypoint.auth.dto
 
-import com.vinicius.challenge.core.domain.client.FavoriteList
-
 data class TokenDto(
     val token: String,
     val clientName: String,
-    val favoriteList: FavoriteList?
+    val favoriteListId: Long?,
+    val clientId: Long
 )
