366879c0a54608158a7e582636a48a50da701c31
Vinicius Hansel <hanelvinicius@gmail.com>
2025-01-29 20:44:25 -0300

    ci(compose.yml) add platform statement to compose

--- src/main/kotlin/com/vinicius/challenge/gateway/product/entity/ProductEntity.kt
+++ src/main/kotlin/com/vinicius/challenge/gateway/product/entity/ProductEntity.kt
@@ -1,11 +1,13 @@
 package com.vinicius.challenge.gateway.product.entity
 
 import com.vinicius.challenge.gateway.client.favorite.entity.FavoriteListEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.Lob
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.SQLRestriction
@@ -20,6 +22,8 @@ class ProductEntity(
     val productId: Long,
     val title: String,
     val price: BigDecimal,
+    @Lob
+    @Column(columnDefinition = "TEXT")
     val description: String,
     val image: String,
     @ManyToOne
